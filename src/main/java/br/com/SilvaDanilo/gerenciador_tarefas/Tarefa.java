b2f97f3fd6fc843bdfbabc481d5f3ba6a61bd9bd
Danilo Silva <danilos282519@ufrrj.br>
2025-07-02 22:34:41 -0300

    Add entity tarefa

---
+++ src/main/java/br/com/SilvaDanilo/gerenciador_tarefas/Tarefa.java
@@ -0,0 +1,34 @@
+package br.com.SilvaDanilo.gerenciador_tarefas;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tarefa")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tarefa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String titulo;
+    private boolean concluida;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataModificacao;
+
+    @PrePersist
+    public void prePersist(){
+        dataCriacao = LocalDateTime.now();
+    }
+    @PreUpdate
+    public void preUpdate(){
+        dataModificacao = LocalDateTime.now();
+    }
+}
+
